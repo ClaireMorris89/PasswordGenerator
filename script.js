1c956d67438667112071b2661aade7c0c0914871
ClaireMorris89 <124100820+ClaireMorris89@users.noreply.github.com>
2023-03-14 13:19:31 -0600

    finalized js

--- script.js
+++ script.js
@@ -43,3 +43,14 @@ function generatePassword(){
    
    return generated;
    };
+
+   // Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+    }
+  
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
